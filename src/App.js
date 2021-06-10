ad8a9192f0a1f1238f71649465339223dd6f627b
Riley McCarthy <mccarthr@uoguelph.ca>
2021-06-10 16:19:19 -0700

    Styled navbar and added jumbotron to home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import brand from "./images/LogoLong.png";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
@@ -8,13 +9,29 @@ import About from "./About.js";
 function App() {
   return (
     <Router>
-      <Navbar scrolling dark expand="md" variant="light" fixed="top">
-        <Navbar.Brand href="/">Home</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/profession">Profession</Nav.Link>
-          <Nav.Link href="/about">About Me</Nav.Link>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-          <Nav.Link href="/social">Social Media</Nav.Link>
+      <Navbar bg="dark" variant="dark" fixed="top">
+        <Navbar.Brand href="/">
+          <img
+            src={brand}
+            height="50"
+            //width="30"
+            className="float-left d-inline-block align-middle"
+            alt="RM"
+          />
+        </Navbar.Brand>
+        <Nav className="ml-auto pl-5">
+          <Nav.Link href="/profession" className="font-weight-bold mr-3">
+            <h3>Profession</h3>
+          </Nav.Link>
+          <Nav.Link href="/about" className="font-weight-bold mr-3">
+            <h3>About Me</h3>
+          </Nav.Link>
+          <Nav.Link href="/projects" className="font-weight-bold mr-3">
+            <h3>Projects</h3>
+          </Nav.Link>
+          <Nav.Link href="/social" className="font-weight-bold mr-3">
+            <h3>Social Media</h3>
+          </Nav.Link>
         </Nav>
       </Navbar>
       <Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,64 @@
 import React, { useEffect } from "react";
-import { Jumbotron, Container } from "react-bootstrap";
+import { Jumbotron, Container, Carousel } from "react-bootstrap";
 import laptopImage from "./images/laptop.jpg";
+import longboardImage from "./images/longboard.jpg";
 
 function Home() {
   useEffect(() => {
     document.title = "Riley McCarthy - Home";
   }, []);
 
+  const carouselImage = {
+    width: "100%",
+    height: "100vh",
+    background: { laptopImage } + " no-repeat center",
+    // backgroundSize: "contain",
+  };
+
   return (
-    <Jumbotron
-      fluid
-      style={{
-        backgroundImage: `url(${laptopImage})`,
-        backgroundSize: "cover",
-        height: "100vh",
-      }}
-    >
-      <Container>
-        <h1>Fluid jumbotron</h1>
-        <p>
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
-        </p>
-      </Container>
-    </Jumbotron>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className=""
+          src={laptopImage}
+          alt="First slide"
+          class="img-fluid"
+          alt="Responsive image"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={longboardImage}
+          alt="Second slide"
+          style={carouselImage}
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={laptopImage}
+          alt="Third slide"
+          style={carouselImage}
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 export default Home;
